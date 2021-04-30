488043b5956bd0c18667292f712bc5a82ab2905a
RewatiRamanPrasad <ramanprasad.0203@gmail.com>
2021-04-30 19:00:33 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using First.Models.Account;
+using System.Web.Security;
+
+
+namespace First.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(User modal)
+        {
+            using (var context=new Database1Entities())
+            {
+                context.Users.Add(modal);
+                context.SaveChanges();
+            
+            }
+                return RedirectToAction("Login");
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            using (var context=new Database1Entities())
+            {
+                bool isValid = context.Users.Any(x => x.Email == user.Email && x.Password == user.Password);
+                if (isValid)
+                {
+                    //FormsAuthentication.SetAuthCookie(user.Name, false);
+                    return RedirectToAction("Index");
+                }
+                ModelState.AddModelError("", "invalid username and password");
+                return View();
+            }
+                
+        }
+
+    }
+}
---
+++ Controllers/SignupController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using First.Models;
+using First.Models.Account;
+
+namespace First.Controllers
+{
+    public class SignupController : Controller
+    {
+        // GET: Signup
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login( User user)
+        {
+            
+                return View();
+        }
+    }
+}
